9d09e79c883087ff3c94a720c582d53195dcc7b3
Sagar Regmi <regmi.sagar@outlook.com>
2024-04-09 12:23:41 +0300

    Project Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 
+import { Routes, Route } from "react-router-dom"
+import Home from "./Pages/Home";
+import Contact from "./Pages/Contact";
+import About from "./Pages/About";
+import Services from "./Pages/Services";
 
 function App() {
   return (
     <div className="App">
-    <h1>Sagar Regmi</h1>
+    <Routes>
+        <Route exact path="/" element={ <Home/> } />
+        <Route exact path="/about" element={ <About/> } />
+        <Route exact path="/contact" element={ <Contact/> } />
+        <Route exact path="/services" element={ <Services/> } />
+      </Routes>
     </div>
   );
 }
